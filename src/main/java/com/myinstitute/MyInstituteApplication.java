7aa92829876468c50cb46926ace065e5848b9a33
Waheed Khan <waheedkhan6670@gmail.com>
2022-10-10 11:17:40 +0500

    First Commit

---
+++ src/main/java/com/myinstitute/MyInstituteApplication.java
@@ -0,0 +1,13 @@
+package com.myinstitute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyInstituteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyInstituteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/myinstitute/MyInstituteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myinstitute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyInstituteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
